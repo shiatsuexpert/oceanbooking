c26a72955f5e01ab6dbc23dc75ea92dd6ada8f00
shiatsuexpert <pet@posteo.org>
2025-12-02 17:32:00 +0100

    fix: Downgrade dependencies for PHP 7.4 support (v1.3.1)

--- ocean-shiatsu-booking.php
+++ ocean-shiatsu-booking.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ocean Shiatsu Booking
  * Plugin URI:  https://oceanshiatsu.com
  * Description: A premium appointment booking system with Google Calendar sync and email workflow.
- * Version:           1.3.0
+ * Version:           1.3.1
  * Author:            Ocean Shiatsu
  * Author URI:        https://oceanshiatsu.com
  * License:           GPL-2.0+
@@ -21,7 +21,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'OCEAN_SHIATSU_BOOKING_VERSION', '1.3.0' );
+define( 'OCEAN_SHIATSU_BOOKING_VERSION', '1.3.1' );
 define( 'OSB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'OSB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
